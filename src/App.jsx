2f4b6bb2efe5fc2924bba5c6733ad3908e66da2b
Kyle <spacemonky@Kyles-MBP.hsd1.ca.comcast.net>
2021-12-25 16:42:09 -0800

    better stlying and instructions

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import { ThirdwebSDK } from "@3rdweb/sdk";
 import { useWeb3 } from "@3rdweb/hooks";
-
+import { ethers } from 'ethers';
 // We instatiate the sdk on Rinkeby.
 const sdk = new ThirdwebSDK("rinkeby");
 
@@ -14,6 +14,10 @@ const tokenModule = sdk.getTokenModule(
   "0x217a70f4c913F49E865FE3D583Dc974802cdC71b"
 );
 
+const voteModule = sdk.getVoteModule(
+  "0x2940f8d0770A3D6640A2a054a9Eb3a90f15635e1",
+);
+
 
 const App = () => {
 
@@ -22,20 +26,18 @@ const App = () => {
 
   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
   const [isClaiming, setIsClaiming] = useState(false);
-  // Holds the amount of token each member has in state.
   const [memberTokenAmounts, setMemberTokenAmounts] = useState({});
-  // The array holding all of our members addresses.
   const [memberAddresses, setMemberAddresses] = useState([]);
-
+  const [proposals, setProposals] = useState([]);
+  const [isVoting, setIsVoting] = useState(false);
+  const [hasVoted, setHasVoted] = useState(false);
 
   // A fancy function to shorten someones wallet address, no need to show the whole thing.
   const shortenAddress = (str) => {
     return str.substring(0, 6) + "..." + str.substring(str.length - 4);
   };
 
-
   // The signer is required to sign transactions on the blockchain.
-  // Without it we can only read data, not write.
   const signer = provider ? provider.getSigner() : undefined;
 
   useEffect(() => {
@@ -69,55 +71,8 @@ const App = () => {
       });
   }, [address]);
 
-
-  if(!address){
-    return(
-      <div>
-        <h1>Welcome to AdhdDAO</h1>
-        <button onClick={() => connectWallet("injected")} className="btn-hero">
-          Connect your wallet
-        </button>
-      </div>
-    );
-  }
-
-
-  // Add this little piece!
-  if (hasClaimedNFT) {
-      return (
-          <div className="member-page">
-            <h1>🍪DAO Member Page</h1>
-            <p>Congratulations on being a member</p>
-            <div>
-              <div>
-                <h2>Member List</h2>
-                <table className="card">
-                  <thead>
-                    <tr>
-                      <th>Address</th>
-                      <th>Token Amount</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {memberList.map((member) => {
-                      return (
-                        <tr key={member.address}>
-                          <td>{shortenAddress(member.address)}</td>
-                          <td>{member.tokenAmount}</td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-    );
-  };
-
-
   // This useEffect grabs all our the addresses of our members holding our NFT.
-useEffect(() => {
+  useEffect(() => {
   if (!hasClaimedNFT) {
     return;
   }
@@ -133,10 +88,10 @@ useEffect(() => {
     .catch((err) => {
       console.error("failed to get member list", err);
     });
-}, [hasClaimedNFT]);
+  }, [hasClaimedNFT]);
 
 // This useEffect grabs the # of token each member holds.
-useEffect(() => {
+  useEffect(() => {
   if (!hasClaimedNFT) {
     return;
   }
@@ -151,10 +106,52 @@ useEffect(() => {
     .catch((err) => {
       console.error("failed to get token amounts", err);
     });
-}, [hasClaimedNFT]);
+  }, [hasClaimedNFT]);
+
+  // Retrieve all our existing proposals from the contract.
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+    // A simple call to voteModule.getAll() to grab the proposals.
+    voteModule
+      .getAll()
+      .then((proposals) => {
+        // Set state!
+        setProposals(proposals);
+        console.log("🌈 Proposals:", proposals)
+      })
+      .catch((err) => {
+        console.error("failed to get proposals", err);
+      });
+  }, [hasClaimedNFT]);
+
+  // We also need to check if the user already voted.
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    // If we haven't finished retrieving the proposals from the useEffect above
+    // then we can't check if the user voted yet!
+    if (!proposals.length) {
+      return;
+    }
+
+    // Check if the user has already voted on the first proposal.
+    voteModule
+      .hasVoted(proposals[0].proposalId, address)
+      .then((hasVoted) => {
+        setHasVoted(hasVoted);
+        console.log("🥵 User has already voted")
+      })
+      .catch((err) => {
+        console.error("failed to check if wallet has voted", err);
+      });
+  }, [hasClaimedNFT, proposals, address]);
 
 // Now, we combine the memberAddresses and memberTokenAmounts into a single array
-const memberList = useMemo(() => {
+  const memberList = useMemo(() => {
   return memberAddresses.map((address) => {
     return {
       address,
@@ -166,9 +163,183 @@ const memberList = useMemo(() => {
       ),
     };
   });
-}, [memberAddresses, memberTokenAmounts]);
+  }, [memberAddresses, memberTokenAmounts]);
 
-const mintNft = () => {
+  if(!address){
+    return(
+      <div className="content-container">
+        <h1>Welcome to AdhdDAO!</h1>
+        <div className="instructions">
+          <h2 style={{'textAlign':'center'}}>An example DAO to learn about DAOs!</h2>
+          <p style={{'textAlign':'center'}}> To use this app all you need to do is get a wallet with a browser extension like <a className="keep-white" href="https://metamask.io/download">MetaMask</a></p>
+          <a  className="instructions-here keep-white" href="https://gist.github.com/tschubotz/8047d13a2d2ac8b2a9faa3a74970c7ef">Follow the Instructions here!</a>
+        </div>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  };
+
+  // If the user has already claimed their NFT we want to display the interal DAO page to them
+   // only DAO members will see this. Render all the members + token amounts.
+   if (hasClaimedNFT) {
+     return (
+       <div className="member-page">
+         <h1>ADHD DAO Member Page</h1>
+         <p>Congratulations on being an attentive member</p>
+         <div>
+           <div>
+             <h2>Member List</h2>
+             <table className="card">
+               <thead>
+                 <tr>
+                   <th>Address</th>
+                   <th>Token Amount</th>
+                 </tr>
+               </thead>
+               <tbody>
+                 {memberList.map((member) => {
+                   return (
+                     <tr key={member.address}>
+                       <td>{shortenAddress(member.address)}</td>
+                       <td>{member.tokenAmount}</td>
+                     </tr>
+                   );
+                 })}
+               </tbody>
+             </table>
+           </div>
+           <div>
+             <h2>Active Proposals</h2>
+             <form
+               onSubmit={async (e) => {
+                 e.preventDefault();
+                 e.stopPropagation();
+
+                 //before we do async things, we want to disable the button to prevent double clicks
+                 setIsVoting(true);
+
+                 // lets get the votes from the form for the values
+                 const votes = proposals.map((proposal) => {
+                   let voteResult = {
+                     proposalId: proposal.proposalId,
+                     //abstain by default
+                     vote: 2,
+                   };
+                   proposal.votes.forEach((vote) => {
+                     const elem = document.getElementById(
+                       proposal.proposalId + "-" + vote.type
+                     );
+
+                     if (elem.checked) {
+                       voteResult.vote = vote.type;
+                       return;
+                     }
+                   });
+                   return voteResult;
+                 });
+
+                 // first we need to make sure the user delegates their token to vote
+                 try {
+                   //we'll check if the wallet still needs to delegate their tokens before they can vote
+                   const delegation = await tokenModule.getDelegationOf(address);
+                   // if the delegation is the 0x0 address that means they have not delegated their governance tokens yet
+                   if (delegation === ethers.constants.AddressZero) {
+                     //if they haven't delegated their tokens yet, we'll have them delegate them before voting
+                     await tokenModule.delegateTo(address);
+                   }
+                   // then we need to vote on the proposals
+                   try {
+                     await Promise.all(
+                       votes.map(async (vote) => {
+                         // before voting we first need to check whether the proposal is open for voting
+                         // we first need to get the latest state of the proposal
+                         const proposal = await voteModule.get(vote.proposalId);
+                         // then we check if the proposal is open for voting (state === 1 means it is open)
+                         if (proposal.state === 1) {
+                           // if it is open for voting, we'll vote on it
+                           return voteModule.vote(vote.proposalId, vote.vote);
+                         }
+                         // if the proposal is not open for voting we just return nothing, letting us continue
+                         return;
+                       })
+                     );
+                     try {
+                       // if any of the propsals are ready to be executed we'll need to execute them
+                       // a proposal is ready to be executed if it is in state 4
+                       await Promise.all(
+                         votes.map(async (vote) => {
+                           // we'll first get the latest state of the proposal again, since we may have just voted before
+                           const proposal = await voteModule.get(
+                             vote.proposalId
+                           );
+
+                           //if the state is in state 4 (meaning that it is ready to be executed), we'll execute the proposal
+                           if (proposal.state === 4) {
+                             return voteModule.execute(vote.proposalId);
+                           }
+                         })
+                       );
+                       // if we get here that means we successfully voted, so let's set the "hasVoted" state to true
+                       setHasVoted(true);
+                       // and log out a success message
+                       console.log("successfully voted");
+                     } catch (err) {
+                       console.error("failed to execute votes", err);
+                     }
+                   } catch (err) {
+                     console.error("failed to vote", err);
+                   }
+                 } catch (err) {
+                   console.error("failed to delegate tokens");
+                 } finally {
+                   // in *either* case we need to set the isVoting state to false to enable the button again
+                   setIsVoting(false);
+                 }
+               }}
+             >
+               {proposals.map((proposal, index) => (
+                 <div key={proposal.proposalId} className="card">
+                   <h5>{proposal.description}</h5>
+                   <div>
+                     {proposal.votes.map((vote) => (
+                       <div key={vote.type}>
+                         <input
+                           type="radio"
+                           id={proposal.proposalId + "-" + vote.type}
+                           name={proposal.proposalId}
+                           value={vote.type}
+                           //default the "abstain" vote to chedked
+                           defaultChecked={vote.type === 2}
+                         />
+                         <label htmlFor={proposal.proposalId + "-" + vote.type}>
+                           {vote.label}
+                         </label>
+                       </div>
+                     ))}
+                   </div>
+                 </div>
+               ))}
+               <button disabled={isVoting || hasVoted} type="submit">
+                 {isVoting
+                   ? "Voting..."
+                   : hasVoted
+                     ? "You Already Voted"
+                     : "Submit Votes"}
+               </button>
+               <small>
+                 This will trigger multiple transactions that you will need to
+                 sign.
+               </small>
+             </form>
+           </div>
+         </div>
+       </div>
+     );
+   };
+
+   const mintNft = () => {
   setIsClaiming(true);
   // Call bundleDropModule.claim("0", 1) to mint nft to user's wallet.
   bundleDropModule
@@ -189,10 +360,11 @@ const mintNft = () => {
   });
 }
 
-// Render mint nft screen.
-return (
+   return (
   <div className="mint-nft">
-    <h1>Mint your free 🍪DAO Membership NFT</h1>
+    <h1>Mint your free ADHD DAO Membership NFT</h1>
+    <p> Make sure MetaMask is on the Rinkeby network so you don't spend real money! </p>
+    <p> Then add "fake" ETH here: <a className="keep-white" href="https://faucets.chain.link/rinkeby">https://faucets.chain.link/rinkeby</a>, it's free! And then claim your NFT! </p>
     <button
       disabled={isClaiming}
       onClick={() => mintNft()}
